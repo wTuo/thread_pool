2eea16d5f80c6ab9641bcc49b06529c771982eac
wxd <wxd.sjtu@foxmail.com>
2018-06-26 22:23:09 +0800

    a more robust version

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,53 +1,80 @@
-#include "thread_pool.h"
+#include <thread_pool.hpp>
 
 
-thread_pool::thread_pool(std::size_t nb_threads):should_stop(false){
-  for(int i=0;i<nb_threads;i++){
-    workers.push_back(std::thread(std::bind(&thread_pool::unit_run, this)));
-  }
+namespace ttp {
+
+thread_pool::thread_pool(std::size_t nb_threads) {
+
+  for (std::size_t i = 0; i < nb_threads; ++i) { m_workers.push_back(std::thread(std::bind(&thread_pool::run, this))); }
 }
 
-
-thread_pool::~thread_pool(void){
+thread_pool::~thread_pool(void) {
   stop();
 }
 
+// worker main loop
+void
+thread_pool::run(void) {
 
-void thread_pool::unit_run(void){
-  while(!should_stop){
+  while (!m_should_stop) {
     task_t task = fetch_task();
-    if(task){
+    if (task) {
       task();
     }
   }
+
 }
 
-void thread_pool::stop(void){
-  should_stop =true;
-  tasks_condvar.notify_all();
+// stop the thread pool and wait for workers completion
 
-  for(auto& worker: workers) {worker.join();}
+void
+thread_pool::stop(void) {
+  if (!is_running()) { return; }
 
-  workers.clear();
+  m_should_stop = true;
+  m_tasks_condvar.notify_all();
+
+  for (auto& worker : m_workers) { worker.join(); }
+
+  m_workers.clear();
+
 }
 
-void thread_pool::add_task(task_t new_task){
-  std::unique_lock<std::mutex> lock(tasks_mtx);
-  tasks.push(new_task);
-  lock.unlock();
-  tasks_condvar.notify_all();
+// whether the thread_pool is running or not
+
+bool
+thread_pool::is_running(void) const {
+  return !m_should_stop;
 }
 
+//! retrieve a new task
 
+thread_pool::task_t
+thread_pool::fetch_task(void) {
+  std::unique_lock<std::mutex> lock(m_tasks_mtx);
 
-task_t thread_pool::fetch_task(){
-  std::unique_lock<std::mutex> lock(tasks_mtx);
-  tasks_condvar.wait(lock, [&]{return should_stop || !tasks.empty();});
+  m_tasks_condvar.wait(lock, [&] { return m_should_stop || !m_tasks.empty(); });
 
+  if (m_tasks.empty()) { return nullptr; }
 
-  if(tasks.empty()){ return nullptr; }
-  task_t task=std::move(tasks.front());
-  tasks.pop();
+  task_t task = std::move(m_tasks.front());
+  m_tasks.pop();
   return task;
 }
 
+// add tasks to thread pool
+
+void
+thread_pool::add_task(const task_t& task) {
+  std::lock_guard<std::mutex> lock(m_tasks_mtx);
+  m_tasks.push(task);
+  m_tasks_condvar.notify_all();
+}
+
+thread_pool&
+thread_pool::operator<<(const task_t& task) {
+  add_task(task);
+  return *this;
+}
+
+} // ttp
